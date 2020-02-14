2ac3885d81946b0889ad6009a8e5b6a4978bed09
mamenaldo <61035755+mamenaldo@users.noreply.github.com>
2020-02-13 21:17:21 -0600

    Add files via upload

---
+++ unidas.js
@@ -0,0 +1,19 @@
+function enviado(){
+    var mensajelement
+    document.getElementById('enviado2').hidden=true
+    mensajelement=document.getElementById('mensaje')
+    mensajelement.innerText="Gracias, te estaremos enviando nuestro newsletter."
+
+}
+
+function formularioMensaje(){
+    var mensajelement
+    document.getElementById("elemento").hidden=true
+    mensajelement=document.getElementById('mensaje')
+    mensajelement.innerText="Gracias, te estaremos enviando más información."
+
+}
+
+
+
+
